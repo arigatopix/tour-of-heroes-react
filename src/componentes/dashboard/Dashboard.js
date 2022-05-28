f0847c19c61eebdb3d38e4c238ae992cfe12f47c
arigatopix <teeruch.k@gmail.com>
2022-05-28 22:20:23 +0700

    refactor dashboard

--- src/componentes/dashboard/Dashboard.js
+++ src/componentes/dashboard/Dashboard.js
@@ -1,5 +1,10 @@
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <h4>Top Heroes</h4>
+      <div className="m2 p-4 text-decoration-none">hero name</div>
+    </>
+  );
 };
 
 export default Dashboard;
