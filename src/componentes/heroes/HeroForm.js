a1ad812788816ee7a15a6abe3c051c3c5c315479
arigatopix <teeruch.k@gmail.com>
2022-05-28 23:51:22 +0700

    add useNavigate for Go back Button

--- src/componentes/heroes/HeroForm.js
+++ src/componentes/heroes/HeroForm.js
@@ -1,23 +1,32 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const HeroForm = () => {
+  const navigate = useNavigate();
+
   const onSubmitHandler = e => {
     e.preventDefault();
+  };
+
+  const onGoBackHandler = () => {
+    navigate(-1);
   };
 
   return (
     <>
       <h4>Hero Name</h4>
       <form onSubmit={onSubmitHandler}>
-        <label class="form-label" for="name">
+        <label className="form-label" htmlFor="name">
           Hero Name
         </label>
-        <input class="form-control" type="text" id="name" />
+        <input className="form-control" type="text" id="name" />
 
         <hr />
-        <div class="d-flex justify-content-between">
-          <button class="btn btn-secondary">Back</button>
-          <button class="btn btn-primary">Save</button>
+        <div className="d-flex justify-content-between">
+          <button className="btn btn-secondary" onClick={onGoBackHandler}>
+            Back
+          </button>
+          <button className="btn btn-primary">Save</button>
         </div>
       </form>
     </>
