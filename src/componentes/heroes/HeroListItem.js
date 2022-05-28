6a5efe8fe1d2ff8259823c367a61bff35e19f10a
arigatopix <teeruch.k@gmail.com>
2022-05-29 00:19:49 +0700

    add onClick event

--- src/componentes/heroes/HeroListItem.js
+++ src/componentes/heroes/HeroListItem.js
@@ -3,21 +3,18 @@ import classes from './HeroListItem.module.css';
 
 const HeroListItem = ({ hero }) => {
   const onDeleteHandler = () => {
-    console.log(hero.id);
+    console.log('Deleted hero id ', hero.id);
   };
 
   return (
-    <li
-      className="list-group-item list-group-item-action"
-      onClick={onDeleteHandler}
-    >
+    <li className="list-group-item list-group-item-action">
       <div className="d-flex justify-content-between">
         <Link to={`/heroes-form/${hero.id}`} className="text-decoration-none">
           <span className={classes.badge}>{hero.id}</span> {hero.name}
         </Link>
-        <div className="btn btn-danger btn-sm">
+        <button className="btn btn-danger btn-sm" onClick={onDeleteHandler}>
           <i className="bi bi-trash"></i>
-        </div>
+        </button>
       </div>
     </li>
   );
