26aa0c9fdd8a494e356e75dc10af6d68f10328a0
arigatopix <teeruch.k@gmail.com>
2022-05-28 23:05:01 +0700

    added Link in HeroListItem

--- src/componentes/heroes/HeroListItem.js
+++ src/componentes/heroes/HeroListItem.js
@@ -1,15 +1,16 @@
+import { Link } from 'react-router-dom';
 import classes from './HeroListItem.module.css';
 
 const HeroListItem = ({ heroes }) => {
   return heroes.map(hero => (
     <li className="list-group-item list-group-item-action" key={hero.id}>
       <div className="d-flex justify-content-between">
-        <div>
+        <Link to={`/heroes-form/${hero.id}`} className="text-decoration-none">
           <span className={classes.badge}>{hero.id}</span> {hero.name}
-        </div>
-        <div className="btn btn-danger btn-sm">
+        </Link>
+        <button className="btn btn-danger btn-sm">
           <i className="bi bi-trash"></i>
-        </div>
+        </button>
       </div>
     </li>
   ));
