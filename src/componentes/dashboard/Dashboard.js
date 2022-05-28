544a356a93b70898532bc7fc74db0ae284c34b8e
arigatopix <teeruch.k@gmail.com>
2022-05-28 23:18:26 +0700

    refactor dashboard and styled

--- src/componentes/dashboard/Dashboard.js
+++ src/componentes/dashboard/Dashboard.js
@@ -1,8 +1,26 @@
+import { Link } from 'react-router-dom';
+import { heroes } from '../mock-heroes';
+
+import classes from './Dashboard.module.css';
+
 const Dashboard = () => {
+  const hero = heroes
+    .map(hero => {
+      return (
+        <Link
+          className={`${classes.hero} m2 p-4 text-decoration-none`}
+          to={`/hero-form/${hero.id}`}
+        >
+          {hero.name}
+        </Link>
+      );
+    })
+    .slice(0, 5);
+
   return (
     <>
       <h4>Top Heroes</h4>
-      <div className="m2 p-4 text-decoration-none">hero name</div>
+      <div className="my-2 d-flex justify-content-around heroes">{hero}</div>
     </>
   );
 };
